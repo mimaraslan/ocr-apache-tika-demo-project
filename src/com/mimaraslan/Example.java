bd279634b79a02b520689eab6356a660f21ff9f7
mimaraslan <mimaraslan@gmail.com>
2020-05-28 17:58:40 +0300

    Apache Tika OCR Demo Java Project
    Apache Tika OCR Demo Java Project

---
+++ src/com/mimaraslan/Example.java
@@ -0,0 +1,9 @@
+package com.mimaraslan;
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("Apache Tika OCR");
+	}
+
+}
--- src/com/mimaraslan/_015_JavaClassParse.java
+++ src/com/mimaraslan/_015_JavaClassParse.java
@@ -1,36 +1,36 @@
 package com.mimaraslan;
 
-	import java.io.File;
-	import java.io.FileInputStream;
-	import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-	import org.apache.tika.exception.TikaException;
-	import org.apache.tika.metadata.Metadata;
-	import org.apache.tika.parser.ParseContext;
-	import org.apache.tika.parser.asm.ClassParser;
-	import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.asm.ClassParser;
+import org.apache.tika.sax.BodyContentHandler;
 
-	import org.xml.sax.SAXException;
+import org.xml.sax.SAXException;
 
-	public class _015_JavaClassParse {
+public class _015_JavaClassParse {
 
-	   public static void main(final String[] args) throws IOException,SAXException, TikaException {
+	public static void main(final String[] args) throws IOException, SAXException, TikaException {
 
-	      //detecting the file type
-	      BodyContentHandler handler = new BodyContentHandler();
-	      Metadata metadata = new Metadata();
-	      FileInputStream inputstream = new FileInputStream(new File("C:\\workspace\\java\\jms-activemq\\ResearchApacheTika\\build\\classes\\com\\mimaraslan\\_015_Example.class"));
-	      ParseContext pcontext = new ParseContext();
+		// detecting the file type
+		BodyContentHandler handler = new BodyContentHandler();
+		Metadata metadata = new Metadata();
+		FileInputStream inputstream = new FileInputStream(new File("Example.class"));
+		ParseContext pcontext = new ParseContext();
 
-	      //Html parser
-	      ClassParser  ClassParser = new  ClassParser();
-	      ClassParser.parse(inputstream, handler, metadata,pcontext);
-	      System.out.println("Contents of the document:" + handler.toString());
-	      System.out.println("Metadata of the document:");
-	      String[] metadataNames = metadata.names();
-	      
-	      for(String name : metadataNames) {		        
-	         System.out.println(name + " :  " + metadata.get(name));  
-	      }
-	   }
+		// Html parser
+		ClassParser ClassParser = new ClassParser();
+		ClassParser.parse(inputstream, handler, metadata, pcontext);
+		System.out.println("Contents of the document:" + handler.toString());
+		System.out.println("Metadata of the document:");
+		String[] metadataNames = metadata.names();
+
+		for (String name : metadataNames) {
+			System.out.println(name + " :  " + metadata.get(name));
+		}
 	}
+}
